edcf1d6298ab6b2979d92b8c0bb6f3b68fd07aab
keytash1 <max.ryabets@mail.ru>
2025-12-26 17:59:07 +0300

    add service logic

--- internal/service/todo_service.go
+++ internal/service/todo_service.go
@@ -30,22 +30,54 @@ func NewTodoService(storage *storage.Storage) *TodoService {
 	return &TodoService{storage: storage}
 }
 
-func (s *TodoService) CreateTodo(req models.CreateTodoInput) (models.Todo, error) {
-
+func (s *TodoService) CreateTodo(req models.CreateTodoInput) (*models.Todo, error) {
+	if req.Title == "" {
+		return nil, &ValidationError{Message: "Title is required"}
+	}
+	todo := &models.Todo{
+		Title:       req.Title,
+		Description: req.Description,
+		Completed:   false,
+	}
+	return s.storage.CreateTodo(todo)
 }
 
-func (s *TodoService) ListTodos() ([]models.Todo, error) {
-
+func (s *TodoService) ListTodos() ([]*models.Todo, error) {
+	return s.storage.ListTodos()
 }
 
-func (s *TodoService) GetTodo(id int) (models.Todo, error) {
-
+func (s *TodoService) GetTodo(id int) (*models.Todo, error) {
+	todo, err := s.storage.GetTodo(id)
+	if err != nil {
+		return nil, err
+	}
+	if todo == nil {
+		return nil, &NotFoundError{ID: id}
+	}
+	return todo, nil
 }
 
 func (s *TodoService) DeleteTodo(id int) error {
-
+	_, err := s.GetTodo(id)
+	if err != nil {
+		return err
+	}
+	return s.storage.DeleteTodo(id)
 }
 
-func (s *TodoService) UpdateTodo(id int, req models.UpdateTodoInput) (models.Todo, error) {
-
+func (s *TodoService) UpdateTodo(id int, req models.UpdateTodoInput) (*models.Todo, error) {
+	if req.Title == "" {
+		return nil, &ValidationError{Message: "Title is required"}
+	}
+	oldtodo, err := s.GetTodo(id)
+	if err != nil {
+		return nil, err
+	}
+	newtodo := &models.Todo{
+		ID:          oldtodo.ID,
+		Title:       req.Title,
+		Description: req.Description,
+		Completed:   req.Completed,
+	}
+	return s.storage.UpdateTodo(id, newtodo)
 }
